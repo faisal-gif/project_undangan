4ee57f0f6f87f184afba4f1d5046c01b9224e456
faisal-gif <fagdera@gmail.com>
2025-11-11 13:41:24 +0700

    modified:   resources/js/Pages/Guest/Welcome/Partials/GuestListNews.jsx

--- resources/js/Pages/Guest/Welcome/Partials/GuestListNews.jsx
+++ resources/js/Pages/Guest/Welcome/Partials/GuestListNews.jsx
@@ -4,19 +4,17 @@ import React from 'react'
 function GuestListNews({ news }) {
     return (
 
-        <section className="pt-10 pb-32 px-6 relative bg-guest-background">
+        <section className=" pb-32 px-6 relative bg-guest-background">
             <div className="absolute inset-0 bg-pattern-dots opacity-30" />
 
-            <div className="text-center mb-20 animate-slide-up">
-                <h2 className="text-2xl md:text-5xl font-display font-bold mb-6 text-gradient-gold">
+            <div className="text-center mb-12 animate-slide-up">
+                <h2 className="text-4xl md:text-5xl font-display font-bold pb-6 text-gradient-gold">
                     Berita Mengenai ATI
                 </h2>
             </div>
 
             <div className="max-w-6xl  mx-auto relative z-10">
-
                 {news.length > 0 && (
-
                     <div className="mb-12 space-y-6 md:grid md:grid-cols-2 md:gap-6 md:space-y-0 lg:grid-cols-3">
                         {news.map((news, index) => (
                             <NewsCardAuto
