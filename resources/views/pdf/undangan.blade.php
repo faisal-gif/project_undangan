93b2332e6ef9b58e3b3a1a3b755b7d1e6bfb0ddf
faisal-gif <fagdera@gmail.com>
2025-11-21 12:09:46 +0700

    modified:   resources/views/pdf/undangan.blade.php

--- resources/views/pdf/undangan.blade.php
+++ resources/views/pdf/undangan.blade.php
@@ -79,7 +79,7 @@
 
         .image {
             position: absolute;
-            bottom: 40mm;
+            bottom: 60mm;
             left: 40%;
             width: 35mm;
             height: 40mm;
